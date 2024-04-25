af880ab715d3e4fafb4b49af7ca1116d4308df4c
whatanx <whatanxx@gmail.com>
2024-04-25 22:10:35 +0200

    Learning about string literals from MDN documenctation

---
+++ MDN/Grammar and Structure/Literals/scripts/app.js
@@ -0,0 +1,18 @@
+// Literal string
+var literalString = `In JavaScript '\n' is a line-feed`;
+
+console.log(literalString);
+
+// Multiple string
+var multipleLineString = `In Js, template strings can run 
+over multiple lines,
+so long live template strings`;
+
+console.log(multipleLineString);
+
+// String interpolation
+const name = "Alex",
+  time = "day before";
+var interpolationString = `Hello ${name}, you need to return your paper: ${time}`;
+
+console.log(interpolationString);
