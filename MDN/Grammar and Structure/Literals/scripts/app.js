0f1d50ce5f4c7113ae191cb72c72dd2bfe8c0d90
whatanx <whatanxx@gmail.com>
2024-04-25 22:49:26 +0200

    So dived into tagged templates for now, used MDN provided script for formatting my args.

--- MDN/Grammar and Structure/Literals/scripts/app.js
+++ MDN/Grammar and Structure/Literals/scripts/app.js
@@ -16,3 +16,37 @@ const name = "Alex",
 var interpolationString = `Hello ${name}, you need to return your paper: ${time}`;
 
 console.log(interpolationString);
+
+// Tagged templates
+// Arrow function to format my arguments
+const formatArg = (arg) => {
+  if (Array.isArray(arg)) {
+    // Mapping
+    return arg.map((part) => `-${part}`).join("\n");
+  }
+  if (arg.toString === Object.prototype.toString) {
+    return JSON.stringify(arg);
+  }
+  return arg;
+};
+
+const print = (segments, ...args) => {
+  let message = segments[0];
+  segments.slice(1).forEach((segment, index) => {
+    message += formatArg(args[index]) + segment;
+  });
+  console.log(message);
+};
+// Smooth Arrray
+const todos = [
+  "Learn JS",
+  "Watch Smallville",
+  "Read Witcher",
+  "Create a project for friend's birthday",
+];
+
+const progress = { js: 10, smallville: 90, witcher: 70, project: 20 };
+print`I need to do:
+${todos}
+My current progress is not so good: ${progress}
+`;
